00a7a5ab1150bc3eb3ebf60714411a072b08285e
nitinatqdesq <nitin@qdesq.com>
2020-09-09 12:22:18 +0530

    data object

--- app/middlewares/auths.js
+++ app/middlewares/auths.js
@@ -8,7 +8,14 @@ const middleware = async ({ body }, res, next) => {
     if (!body.email || !body.password)
       return res.status(400).json({ error: true, message: 'credentials are missing' })
 
+    // mock data
     const userFound = {
+      first_name: 'Nitin ',
+      last_name: 'Manocha ',
+      email: 'manochanitin99@gmail.com ',
+      created_at: '2020-09-09 ',
+      deleted_at: null,
+      updated_at: '2020-09-09 ',
       password_hash: "$2b$09$j9fklGLSJKEFDr4IWSwAHeFL7aEsg9ghvl7VFBYCKyr7JTVF6TCXm", // qwerty
       active: true
     } // here we will fetch the user from db
